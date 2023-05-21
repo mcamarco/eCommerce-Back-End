027051465d9fb9a8076aeb0f6c55fa8e0bf52dba
Mickey Camarco <48422179+mcamarco@users.noreply.github.com>
2023-05-20 23:12:23 -0400

    updated product.js, tested and confirmed success

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -1,15 +1,18 @@
-// import important parts of sequelize library
 const { Model, DataTypes } = require("sequelize");
-// import our database connection from config.js
 const sequelize = require("../config/connection");
 
-// Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model {
+  static async updateProduct(id, updatedData) {
+    const [rowsAffected, [updatedProduct]] = await this.update(updatedData, {
+      where: { id },
+      returning: true,
+    });
+    return { rowsAffected, updatedProduct };
+  }
+}
 
-// set up fields and rules for Product model
 Product.init(
   {
-    // define columns
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -49,7 +52,3 @@ Product.init(
 );
 
 module.exports = Product;
-
-// Product belongs to Category, and Category has many Product models, as a category can have multiple products but a product can only belong to one category.
-
-// Product belongs to many Tag models, and Tag belongs to many Product models. Allow products to have multiple tags and tags to have many products by using the ProductTag through model.
